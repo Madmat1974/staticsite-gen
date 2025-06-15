497479ee72e3d5e2b362ece4e7d5c04e8e0e29da
Martin <MartinNiswonger@gmail.com>
2025-06-15 08:50:09 -0500

    ch5: l1 complete

--- src/main.py
+++ src/main.py
@@ -1,6 +1,28 @@
+import os, shutil
 from textnode import *
+
+import os, shutil
+
+def copy_file_function(source, destination):
+    items = os.listdir(source)
+    
+    for item in items:
+        full_source_path = os.path.join(source, item)
+        full_destination_path = os.path.join(destination, item)
+        print(f"Full destination path: {full_destination_path}")
+
+        if os.path.isfile(full_source_path):
+            shutil.copy(full_source_path, full_destination_path)
+            print(f"copying {item} from {source} to {destination}")
+        else:
+            os.mkdir(full_destination_path)
+            print("making directory")
+            copy_file_function(full_source_path, full_destination_path)
+
 def main():
-   testing = TextNode("This is a test of my incompetence", TextType.BOLD, "https://www.boot.dev")
-   print(testing)
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    os.mkdir("public")
+    copy_file_function("static", "public")
 
 main()
